9b3616f66d8a0cda6173adf6fee1926c1a321715
annekurian <anne.kurian14@gmail.com>
2026-07-06 23:14:49 +0530

    Adding templates and database integration for View All Books and View All Authors

--- python-flask-sqlite3/app.py
+++ python-flask-sqlite3/app.py
@@ -11,8 +11,20 @@ def index():
 
 @app.route('/viewAllAuthors', methods=['GET'])
 def viewAllAuthors():
-    return render_template('authors.html')
+    con=sqlite3.connect("./db/books.db")
+    cursor = con.cursor()
+    cursor.execute('SELECT * from authors')
+    results = cursor.fetchall()
+    cursor.close()
+
+    return render_template('authors.html', authors=results)
 
 @app.route('/viewAllBooks', methods=['GET'])
 def viewAllBooks():
-    return render_template('books.html')
+    con=sqlite3.connect("./db/books.db")
+    cursor = con.cursor()
+    cursor.execute('SELECT b.id, b.title as title, a.name as author, g.name as genre FROM books b, authors a, genres g WHERE b.author_id = a.id AND b.genre_id = g.id')
+    results = cursor.fetchall()
+    cursor.close()
+
+    return render_template('books.html', books=results)
