0f126b386e17587e4faa3d1118e70ffc6b6ace73
annekurian <anne.kurian14@gmail.com>
2026-07-06 22:20:53 +0530

    Added endpoints and basic html for all endpoints

--- python-flask-sqlite3/app.py
+++ python-flask-sqlite3/app.py
@@ -8,3 +8,11 @@ app=Flask(__name__)
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/viewAllAuthors', methods=['GET'])
+def viewAllAuthors():
+    return render_template('authors.html')
+
+@app.route('/viewAllBooks', methods=['GET'])
+def viewAllBooks():
+    return render_template('books.html')
