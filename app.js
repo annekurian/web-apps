f633d9f5355c13585c2a195d48e54512b9fdf5b2
annekurian <anne.kurian14@gmail.com>
2026-07-06 16:34:44 +0530

    Setup SQLite3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./db/books.db");
 
 app.set("view engine", "pug");
 
